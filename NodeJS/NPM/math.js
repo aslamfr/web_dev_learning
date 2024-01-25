124633da25583f118caeedaff61acae056774d01
aslamfr <afafathin@Gmail.com>
2024-01-25 08:59:59 +0700

    updated NodeJS

---
+++ NodeJS/NPM/math.js
@@ -0,0 +1,15 @@
+module.exports.add = (x,y) => x+y;
+module.exports.PI = 3.14159;
+module.exports.square = x => x*x;
+
+// const math = {
+//   add: add,
+//   PI: PI,
+//   square: square
+// }
+
+// module.exports = "This is custom Math module";
+// module.exports.add = add;
+// module.exports.PI = PI;
+// module.exports.square = square;
+// module.exports.math;;
